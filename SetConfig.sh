1b047380f7e42ee376502e88706ce0b66e757fe5
savoptik <sav70792@gmail.com>
2020-01-21 16:13:20 +0400

    Add target for releas and debug configuration JDK

---
+++ SetConfig.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#  SetConfig.sh
+#  JetBrainsRuntime_XCode_project
+#
+#  Created by Артём Семёнов on 21.01.2020.
+#  Copyright © 2020 Артём Семёнов. All rights reserved.
+
+if [ -e ./JetBrainsRuntime/README ]
+then
+cd ./JetBrainsRuntime/ &&
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors
+else
+git sumbmodule init
+git sumbmodule update
+cd ./JetBrainsRuntime/ &&
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors
+fi
