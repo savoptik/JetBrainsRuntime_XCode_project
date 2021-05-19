c3f05eb903e65b2aa4a7554b17c86e9f7074b64e
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-05-19 19:09:41 +0300

    Изменения в проекте

--- SetConfig.sh
+++ SetConfig.sh
@@ -6,20 +6,20 @@
 #  Created by Артём Семёнов on 21.01.2020.
 #  Copyright © 2020 Артём Семёнов. All rights reserved.
 
-if [ -e ./JetBrainsRuntime/README ]
+if [ -e ./jdk/README.md ]
 then
-cd ./JetBrainsRuntime/ &&
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors
-make CONF=macosx-x86_64-normal-server-fastdebug clean
-make CONF=macosx-x86_64-normal-server-release clean
+cd ./jdk/ &&
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors
+make CONF=macosx-x86_64-server-fastdebug clean
+make CONF=macosx-x86_64-server-release clean
 else
 git submodule init
 git submodule update
-cd ./JetBrainsRuntime/ &&
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-11.0.5.jdk/Contents/Home --disable-warnings-as-errors
-make CONF=macosx-x86_64-normal-server-fastdebug clean
-make CONF=macosx-x86_64-normal-server-release clean
+cd ./jdk/ &&
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors
+make CONF=macosx-x86_64-server-fastdebug clean
+make CONF=macosx-x86_64-server-release clean
 fi
 
