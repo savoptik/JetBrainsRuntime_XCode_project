e432657f2c0663ae6732e33edab5a6e23f8b3ebf
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-06-01 13:34:19 +0300

    JDK-8267385 Create NSAccessibilityElement implementation for JavaComponentAccessibility

--- SetConfig.sh
+++ SetConfig.sh
@@ -10,7 +10,7 @@ if [ -e ./jdk/README.md ]
 then
 cd ./jdk/ &&
 bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home # --disable-warnings-as-errors
 make CONF=macosx-x86_64-server-fastdebug clean
 make CONF=macosx-x86_64-server-release clean
 else
@@ -18,7 +18,7 @@ git submodule init
 git submodule update
 cd ./jdk/ &&
 bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors --enable-debug;
-bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home --disable-warnings-as-errors
+bash configure --with-boot-jdk=/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home # --disable-warnings-as-errors
 make CONF=macosx-x86_64-server-fastdebug clean
 make CONF=macosx-x86_64-server-release clean
 fi
